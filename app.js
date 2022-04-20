85eac35676ce99bfab4529c0a3e0500979b4d6e7
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2022-04-20 17:48:39 +0700

    add

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", (req, res) => {
-  res.send("hello");
-});
-
 app.use("/api/v1/contact", contactRouter);
 
 module.exports = app;
